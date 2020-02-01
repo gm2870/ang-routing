898fce62a887e57f4a4c7f414459bc54ac28ff78
Ali Farzanegan <A.Farzanegan@Son.ir>
2020-02-01 09:22:38 +0330

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import {Routes,RouterModule} from "@angular/router"
+const appRoutes :Routes = [
+  {
+    path:"",
+    component:HomeComponent
+  },
+  {
+    path:"users",
+    component:UsersComponent
+  },
+  {
+    path:"servers",
+    component:ServersComponent
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    ServerComponent,
+    EditServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
